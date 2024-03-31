b913cfe2e63dffb7261b838f72b70e9f00856395
Pranav AVN <pranavavn@outlook.com>
2024-03-31 20:28:36 +0530

    Accept Input

--- hworld.asm
+++ hworld.asm
@@ -1,18 +1,48 @@
-.global _start
-.intel_syntax noprefix
+SYS_READ   equ     0          ; read text from stdin
+SYS_WRITE  equ     1          ; write text to stdout
+SYS_EXIT   equ     60         ; terminate the program
+STDIN      equ     0          ; standard input
+STDOUT     equ     1          ; standard output
+
+section .bss
+    uinput_len equ     24         ; 24 bytes for user input
+    uinput     resb    uinput_len ; buffer for user input
+
+section .data
+    prompt     db      "Please input some text: "
+    prompt_len equ     $ - prompt
+    text       db      10, "You wrote: "
+    text_len   equ     $ - text
+
+section .text
+    global _start
+
 _start:
-    // sys_write
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [hello_world]
-    mov rdx, 14
+    mov     rdx, prompt_len
+    mov     rsi, prompt
+    mov     rdi, STDOUT
+    mov     rax, SYS_WRITE
     syscall
 
-    
-    // sys_exit
-    mov rax, 60
-    mov rdi, 69
+    mov     rdx, uinput_len
+    mov     rsi, uinput
+    mov     rdi, STDIN
+    mov     rax, SYS_READ
+    syscall                      ; -> RAX
+    push    rax                  ; (1)
+
+    mov     rdx, text_len
+    mov     rsi, text
+    mov     rdi, STDOUT
+    mov     rax, SYS_WRITE
     syscall
 
-hello_world:
-    .asciz "Hello, World!\n"
+    pop     rdx                  ; (1)
+    mov     rsi, uinput
+    mov     rdi, STDOUT
+    mov     rax, SYS_WRITE
+    syscall
+
+    xor     edi, edi             ; successful exit
+    mov     rax, SYS_EXIT
+    syscall
